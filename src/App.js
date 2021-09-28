1005e89e9e03b1b0967cfc5b77e8064b4f13f7dc
Junnior Villanueva <junnior.com.pe@outlook.com>
2021-09-28 16:14:16 -0500

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>Tareas</h1>
+      <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <span className="lead">Nombre de la tarea</span>
+              <button className="btn btn-danger btn-sm float-right mx-2">Editar</button>
+              <button className="btn btn-warning btn-sm float-right mx-2">Eliminar</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+        </div>        
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
